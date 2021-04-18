eb71d38fd049975013baf13794770050bd866b26
Pavel Mironchik <mairon-pasha@yandex.ru>
2021-04-18 20:04:41 +0300

    add lighting; refractor code: add edges, normales

--- drawer.cpp
+++ drawer.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 
 // --------------- temp code --------------------
+const QVector4D figureColor = QVector4D(1.0f, 0.0f, 0.0f, 1.0f);
+const QVector3D lightPosition = QVector3D(5.0f, 10.0f, 40.0f);
+const QVector3D lightColor = QVector3D(1.0f, 1.0f, 1.0f);
+
 QVector3D const vertices[] = {
     QVector3D(1, 1, -1),
     QVector3D(-1, 1, -1),
@@ -13,27 +17,52 @@ QVector3D const vertices[] = {
     QVector3D(1, -1, 1),
 };
 
-const int edgesCount = 6 * 4;
-int const edges[] = {
-    0, 3, 2, 1,
-    4, 5, 6, 7,
-    5, 1, 2, 6,
-    2, 3, 7, 6,
-    0, 4, 7, 3,
-    0, 1, 5, 4,
+// counterclock-wise vertices in edges
+const QVector<Edge> edges = {
+    {{vertices + 0,
+      vertices + 3,
+      vertices + 2,
+      vertices + 1}, 1},
+    {{vertices + 4,
+      vertices + 5,
+      vertices + 6,
+      vertices + 7}, 2},
+    {{vertices + 5,
+      vertices + 1,
+      vertices + 2,
+      vertices + 6}, 3},
+    {{vertices + 2,
+      vertices + 3,
+      vertices + 7,
+      vertices + 6}, 4},
+    {{vertices + 0,
+      vertices + 4,
+      vertices + 7,
+      vertices + 3}, 5},
+    {{vertices + 0,
+      vertices + 1,
+      vertices + 5,
+      vertices + 4}, 6},
 };
 
 // length for cube: 6 edges, 6 vertices in edge (2 triangles)
 const int bufferLength = 6 * 6;
 
-QVector3D * writeEdge(QVector3D *target, const int *edge) {
-    target[0] = vertices[edge[0]];
-    target[1] = vertices[edge[2]];
-    target[2] = vertices[edge[1]];
-    target[3] = vertices[edge[0]];
-    target[4] = vertices[edge[3]];
-    target[5] = vertices[edge[2]];
-    return target + 6;
+void writeEdge(QVector<VertexData>& arrayBuffer, QVector<int>& indexBuffer, const Edge* edge) {
+    QVector3D normal = edge->normal();
+    int startIndex = arrayBuffer.size();
+    for (int i = 0; i < edge->vertices.size(); i++) {
+        VertexData data;
+        data.vertex = *edge->vertices[i];
+        data.color = figureColor;
+        data.normal = normal;
+        arrayBuffer.append(data);
+    }
+    for (int i = 2; i < edge->vertices.size(); i++) {
+        indexBuffer.append(startIndex);
+        indexBuffer.append(startIndex + i - 1);
+        indexBuffer.append(startIndex + i);
+    }
 }
 
 // --------------- temp code end ----------------
@@ -41,8 +70,6 @@ QVector3D * writeEdge(QVector3D *target, const int *edge) {
 Drawer::Drawer(QWidget *parent) : QOpenGLWidget(parent)
 {
     setMouseTracking(true);
-    initMatrices();
-    initBuffer();
 }
 
 Drawer::~Drawer() {
@@ -52,13 +79,17 @@ Drawer::~Drawer() {
 
 void Drawer::initBaseProgram() {
     _baseProgram = new QOpenGLShaderProgram(this);
-    _baseProgram->addShaderFromSourceFile(QOpenGLShader::Vertex, ":/res/base.vert");
+    if (!_baseProgram->addShaderFromSourceFile(QOpenGLShader::Vertex, ":/res/base.vert")) {
+        std::cout << "Cannot compile vertex shader" << std::endl;
+        std::cout << _baseProgram->log().data() << std::endl;
+        return;
+    }
     _baseProgram->addShaderFromSourceFile(QOpenGLShader::Fragment, ":/res/base.frag");
 
     if (_baseProgram->link()) {
-        std::cout << "Shaders program was linked";
+        std::cout << "Shaders program was linked" << std::endl;
     } else {
-        std::cout << "Cannot link shaders program";
+        std::cout << "Cannot link shaders program" << std::endl;
     }
 }
 void Drawer::initMatrices() {
@@ -69,16 +100,28 @@ void Drawer::initMatrices() {
     _view.lookAt(QVector3D(4, 4, 4), QVector3D(0, 0, 0), QVector3D(0, 0, 1));
 }
 
-void Drawer::initBuffer() {
-    _buffer = new QVector3D[bufferLength];
-    QVector3D *tmp = _buffer;
-    for (int i = 0; i < edgesCount; i += 4) {
-        tmp = writeEdge(tmp, edges + i);
+void Drawer::initArrays() {
+    _verticesArray.clear();
+    _indexesArray.clear();
+    for (const Edge& edge : edges) {
+        writeEdge(_verticesArray, _indexesArray, &edge);
     }
 }
 
 void Drawer::initializeGL() {
     initBaseProgram();
+    initMatrices();
+    initArrays();
+
+    _verticesArrayBuffer.create();
+    _verticesArrayBuffer.bind();
+    _verticesArrayBuffer.allocate(_verticesArray.constData(), _verticesArray.size() * sizeof(VertexData));
+    _verticesArrayBuffer.release();
+
+    _indexesArrayBuffer.create();
+    _indexesArrayBuffer.bind();
+    _indexesArrayBuffer.allocate(_indexesArray.constData(), _indexesArray.size() * sizeof(GLuint));
+    _indexesArrayBuffer.release();
 }
 
 void Drawer::resizeGL(int w, int h) {
@@ -88,26 +131,50 @@ void Drawer::resizeGL(int w, int h) {
 
 void Drawer::paintGL() {
     if (!_baseProgram->isLinked()) {
-        std::cout << "Shaders are not linked; can not paint";
+        std::cout << "Shaders are not linked; can not paint" << std::endl;
         return;
     }
 
     if (!_baseProgram->bind()) {
-        std::cout << "Cannot bind shaders; can not paint";
+        std::cout << "Cannot bind shaders; can not paint" << std::endl;
         return;
     }
+    glEnable(GL_DEPTH_TEST);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     int projLoc = _baseProgram->uniformLocation("proj");
     int viewLoc = _baseProgram->uniformLocation("view");
     int modelLoc = _baseProgram->uniformLocation("model");
-    int vertexLocation = _baseProgram->attributeLocation("vertex");
+    int lightColorLocation = _baseProgram->uniformLocation("lightColor");
+    int lightPositionLocation = _baseProgram->uniformLocation("lightPosition");
     _baseProgram->setUniformValue(projLoc, _proj);
     _baseProgram->setUniformValue(viewLoc, _view);
     _baseProgram->setUniformValue(modelLoc, _model);
+    _baseProgram->setUniformValue(lightColorLocation, lightColor);
+    _baseProgram->setUniformValue(lightPositionLocation, lightPosition);
+
+
+    int vertexLocation = _baseProgram->attributeLocation("vertex");
+    int colorLocation = _baseProgram->attributeLocation("color");
+    int normalLocation = _baseProgram->attributeLocation("normal");
+
+    _verticesArrayBuffer.bind();
+
+    int offset = 0;
     _baseProgram->enableAttributeArray(vertexLocation);
-    _baseProgram->setAttributeArray(vertexLocation, _buffer);
-    glDrawArrays(GL_TRIANGLES, 0, bufferLength);
-    _baseProgram->disableAttributeArray(vertexLocation);
+    _baseProgram->setAttributeBuffer(vertexLocation, GL_FLOAT, offset, 3, sizeof(VertexData));
+
+    offset += sizeof(QVector3D);
+    _baseProgram->enableAttributeArray(colorLocation);
+    _baseProgram->setAttributeBuffer(colorLocation, GL_FLOAT, offset, 4, sizeof(VertexData));
+
+    offset += sizeof(QVector4D);
+    _baseProgram->enableAttributeArray(normalLocation);
+    _baseProgram->setAttributeBuffer(normalLocation, GL_FLOAT, offset, 3, sizeof(VertexData));
+
+    _indexesArrayBuffer.bind();
+
+    glDrawElements(GL_TRIANGLES, _indexesArray.size(), GL_UNSIGNED_INT, 0);
 }
 
 void Drawer::mouseMoveEvent(QMouseEvent* event) {
--- drawer.h
+++ drawer.h
@@ -1,7 +1,26 @@
 #pragma once
 
+#include <QOpenGLBuffer>
 #include <QOpenGLShaderProgram>
 #include <QOpenGLWidget>
+
+struct Edge {
+    QVector<const QVector3D*> vertices;
+    int id;
+
+    QVector3D normal() const {
+        assert(vertices.size() >= 3);
+        QVector3D v1 = *vertices[2] - *vertices[1],
+                v2 = *vertices[0] - *vertices[1];
+        return QVector3D::crossProduct(v1, v2);
+    }
+};
+
+struct VertexData {
+    QVector3D vertex;
+    QVector4D color;
+    QVector3D normal;
+};
 
 class Drawer : public QOpenGLWidget
 {
@@ -24,10 +43,14 @@ private:
     QMatrix4x4 _proj;
     QMatrix4x4 _view;
     QMatrix4x4 _model;
-    QVector3D *_buffer;
+    QVector<VertexData> _verticesArray;
+    QVector<int> _indexesArray;
+    QOpenGLBuffer _verticesArrayBuffer = QOpenGLBuffer(QOpenGLBuffer::VertexBuffer);
+    QOpenGLBuffer _indexesArrayBuffer = QOpenGLBuffer(QOpenGLBuffer::IndexBuffer);
+
 
     void initBaseProgram();
     void initMatrices();
-    void initBuffer();
+    void initArrays();
 };
 
